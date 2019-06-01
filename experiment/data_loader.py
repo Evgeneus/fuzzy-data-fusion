2e913116a0740953b6f6e1cc6cb8a919ea00b674
Evgeny Krivosheev <K1r9i9v3osheev>
2019-06-01 15:09:14 +0200

    feature/update load_data_food

--- experiment/data_loader.py
+++ experiment/data_loader.py
@@ -122,39 +122,63 @@ def load_data_food(truncater=None):
     test = [[], []]
     f1_df = pd.read_csv('../data/Food/food1_res.csv', delimiter=';')
     f2_df = pd.read_csv('../data/Food/food2_res.csv', delimiter=';')
+    f3_df = pd.read_csv('../data/Food/food3_res.csv', delimiter=';')
     ## remove rows with "I don't know" votes
     f1_df = f1_df[f1_df['crowd_ans'] != "I don't know"]
     f2_df = f2_df[f2_df['crowd_ans'] != "I don't know"]
+    f3_df = f3_df[f3_df['crowd_ans'] != "I don't know"]
     if truncater is not None:
         f1_df = truncater.do_trancate(f1_df)
         f2_df = truncater.do_trancate(f2_df)
+        f3_df = truncater.do_trancate(f3_df)
 
     s_f1 = set(f1_df['_worker_id'].values)
     s_f2 = set(f2_df['_worker_id'].values)
-    sources = s_f1 | s_f2
+    s_f3 = set(f3_df['_worker_id'].values)
+    sources = s_f1 | s_f2 | s_f3
     source_dict = dict(zip(sources, range(len(sources))))
     N = len(source_dict)  # number of sources
-    M = 20  # number of objects
+    M = 20  # number of objects 10+10
+    M2 = 56  # number of objects df3: 56
 
     Cl, GT = {}, {}
+    ## df1, df2
     for i in range(M / 2):
         GT[i] = f1_df[f1_df['question_n'] == i+1]['gt'].values[0]
         GT[i + M/2] = f2_df[f2_df['question_n'] == i+1]['gt'].values[0]
         Cl.update({i: {'id': i, 'other': i + M/2}})
         Cl.update({i + M/2: {'id': i + M/2, 'other': i}})
+    ## df3
+    for i in range(M, M + M2 / 2 / 2):
+        GT[i] = f3_df[f3_df['question_n'] == i-M+1]['gt'].values[0]
+        GT[i + M2/2/2] = f3_df[f3_df['question_n'] == i-M+1+M2/2/2]['gt'].values[0]
+        Cl.update({i: {'id': i, 'other': i + M2/2/2}})
+        Cl.update({i + M2/2/2: {'id': i + M2/2/2, 'other': i}})
+    for i in range(M + M2 / 2, M + 3*M2/4):
+        GT[i] = f3_df[f3_df['question_n'] == i-M+1]['gt'].values[0]
+        GT[i + M2/2/2] = f3_df[f3_df['question_n'] == i-M+1+M2/2/2]['gt'].values[0]
+        Cl.update({i: {'id': i, 'other': i + M2/2/2}})
+        Cl.update({i + M2/2/2: {'id': i + M2/2/2, 'other': i}})
 
     GT_G = {}
-    for obj in range(M):
+    for obj in range(M+M2):
         GT_G[obj] = {}
 
     conf_counter = 0
     total_votes = 0
-    Psi = [[] for _ in range(M)]
-    for obj_id in range(M):
+    Psi = [[] for _ in range(M+M2)]
+    for obj_id in range(M+M2):
         if obj_id < M/2:
             obj_data = f1_df.loc[f1_df['question_n'] == obj_id+1]
-        else:
+        elif obj_id >= M/2 and obj_id < M:
             obj_data = f2_df.loc[f2_df['question_n'] == obj_id-M/2+1]
+        else:   ## obj_id >= 20 and obj_id < 20+28
+        # elif obj_id >= M and obj_id < M + M2 /2:   ## obj_id >= 20 and obj_id < 20+28
+            obj_data = f3_df.loc[f3_df['question_n'] == obj_id-M+1]
+        # else:  ## obj_id >= M + M2 /2 ()
+        #     pass
+
+
         for index, row in obj_data.iterrows():
             s_id = source_dict[row['_worker_id']]
             vote = row['crowd_ans']
@@ -171,8 +195,8 @@ def load_data_food(truncater=None):
             else:
                 GT_G[obj_id][s_id] = 1
             total_votes += 1
-    # print '#confusions: {}, {:1.1f}%'.format(conf_counter, conf_counter*100./total_votes)
-    # print '#total votes: {}'.format(total_votes)
+    print '#confusions: {}, {:1.1f}%'.format(conf_counter, conf_counter*100./total_votes)
+    print '#total votes: {}'.format(total_votes)
     return [N, M, Psi, GT, Cl, GT_G]
 
 
